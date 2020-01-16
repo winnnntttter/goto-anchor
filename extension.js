e08aa9bcea80fde72678e186f2a034cbd32ccf52
winnnntttter <1013222027@qq.com>
2020-01-16 17:26:16 +0800

    基本功能

--- extension.js
+++ extension.js
@@ -4,36 +4,56 @@ let anchors = {
   stamp2: {},
   stamp3: {},
   stamp4: {},
-  stamp5: {}
+  stamp5: {},
+  stamp6: {},
+  stamp7: {},
+  stamp8: {},
+  stamp9: {},
+  stamp0: {}
 };
+let seq = 0;
+function encodeLocation(uri, pos) {
+  /* console.log(JSON.stringify([vscode.Uri.file(uri).toString(), pos.line, pos.character]))
+  const query = JSON.stringify([vscode.Uri.file(uri).toString(), pos.line, pos.character]);
+  return vscode.Uri.file(`${query}#${seq++}`); */
+  return vscode.Uri.file(uri);
+  // return vscode.Uri.parse(uri);
+}
 function activate(context) {
-  console.log('Congratulations, your extension "goto-anchor" is now active!');
-  let disposable = vscode.commands.registerCommand("gotoAnchor.stamp1", function() {
+  function setStamp(index) {
     var editor = vscode.window.activeTextEditor;
     if (!editor) {
-      console.log("aaa");
       vscode.window.showInformationMessage("Hello World!");
     } else {
-      anchors.stamp1.line = editor.selection.active.line;
-      anchors.stamp1.character = editor.selection.active.character;
-      anchors.stamp1.docName = editor.selection.active.docName;
+      anchors["stamp" + index].line = editor.selection.active.line;
+      anchors["stamp" + index].character = editor.selection.active.character;
+      anchors["stamp" + index].docName = editor.document.fileName;
     }
-    vscode.window.showInformationMessage("Hello World!");
-  });
-  let disposable2 = vscode.commands.registerCommand("gotoAnchor.stamp2", function() {
-    var editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      console.log("aaa");
-      vscode.window.showInformationMessage("Hello World!");
-    } else {
-      anchors.stamp1.line = editor.selection.active.line;
-      anchors.stamp1.character = editor.selection.active.character;
-      anchors.stamp1.docName = editor.selection.active.docName;
+    vscode.window.showInformationMessage("Hello World!" + index);
+  }
+  function go(index) {
+    if (anchors["stamp" + index].docName) {
+      const uri = encodeLocation(anchors["stamp" + index].docName, anchors["stamp" + index]);
+      console.log(uri);
+      vscode.workspace.openTextDocument(uri).then(doc =>
+        vscode.window.showTextDocument(doc, {
+          selection: new vscode.Range(new vscode.Position(anchors["stamp" + index].line, anchors["stamp" + index].character), new vscode.Position(anchors["stamp" + index].line, anchors["stamp" + index].character)),
+          preview: false
+        })
+      );
     }
-    vscode.window.showInformationMessage("Hello World!");
-  });
-  context.subscriptions.push(disposable);
-  context.subscriptions.push(disposable2);
+    vscode.window.showInformationMessage("Hello World!aaa" + index);
+  }
+  for (let i = 0; i < 10; i++) {
+    let disposable = vscode.commands.registerCommand("gotoAnchor.stamp" + i, function() {
+      setStamp(i);
+    });
+    let disposable2 = vscode.commands.registerCommand("gotoAnchor.go" + i, function() {
+      go(i);
+    });
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
+  }
 }
 exports.activate = activate;
 
